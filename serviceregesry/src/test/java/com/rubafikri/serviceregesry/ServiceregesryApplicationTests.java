ed2e9caa0894be4acbb0003d32d653a8d0ab17e7
our ame <rubafikri@gmail.com>
2021-12-18 15:26:45 +0200

    first commit

---
+++ serviceregesry/src/test/java/com/rubafikri/serviceregesry/ServiceregesryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubafikri.serviceregesry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregesryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
